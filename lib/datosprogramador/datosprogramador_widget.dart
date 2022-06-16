0a883f7724f69e3d71f7a40436829c4536473c06
FlutLab.io <>
2022-06-16 13:44:58 +0000

    Initial commit

---
+++ lib/datosprogramador/datosprogramador_widget.dart
@@ -0,0 +1,144 @@
+import '../flutter_flow/flutter_flow_icon_button.dart';
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import '../menu/menu_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DatosprogramadorWidget extends StatefulWidget {
+  const DatosprogramadorWidget({Key key}) : super(key: key);
+
+  @override
+  _DatosprogramadorWidgetState createState() => _DatosprogramadorWidgetState();
+}
+
+class _DatosprogramadorWidgetState extends State<DatosprogramadorWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0E5C36),
+        automaticallyImplyLeading: false,
+        leading: FlutterFlowIconButton(
+          borderColor: Colors.transparent,
+          borderRadius: 30,
+          borderWidth: 1,
+          buttonSize: 60,
+          icon: Icon(
+            Icons.chevron_left,
+            color: FlutterFlowTheme.of(context).lineColor,
+            size: 30,
+          ),
+          onPressed: () async {
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MenuWidget(),
+              ),
+            );
+          },
+        ),
+        title: Text(
+          'Datos del programador',
+          style: FlutterFlowTheme.of(context).title2.override(
+                fontFamily: 'Poppins',
+                color: Colors.white,
+                fontSize: 22,
+              ),
+        ),
+        actions: [],
+        centerTitle: false,
+        elevation: 2,
+      ),
+      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 50, 0, 20),
+                child: Material(
+                  color: Colors.transparent,
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Container(
+                    width: 280,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFEEEEEE),
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Color(0xFF0E5C36),
+                        width: 2,
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Align(
+                          alignment: AlignmentDirectional(0, 0),
+                          child: Text(
+                            'Moreno Melendez Carlos Alberto\n',
+                            style:
+                                FlutterFlowTheme.of(context).bodyText1.override(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 16,
+                                    ),
+                          ),
+                        ),
+                        Text(
+                          '6-I',
+                          style:
+                              FlutterFlowTheme.of(context).bodyText1.override(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 16,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Divider(
+                thickness: 5,
+                indent: 50,
+                endIndent: 50,
+                color: Color(0xFF0E5C36),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0, 0),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                  child: Container(
+                    width: 120,
+                    height: 120,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.network(
+                      'https://raw.githubusercontent.com/CarlosAlbertoMorenoMelendez/IMSS_APP/master/assets/images/me.jpg',
+                    ),
+                  ),
+                ),
+              ),
+              Divider(
+                thickness: 5,
+                indent: 50,
+                endIndent: 50,
+                color: Color(0xFF0E5C36),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
